c4a0a2fcb3066a1ff76363366a67e425fd8d12a8
andrewliu26 <aliu26@bu.edu>
2023-10-13 21:11:31 -0400

    basic layout with SeekBar

--- app/src/main/java/com/example/accelerometer/MainActivity.kt
+++ app/src/main/java/com/example/accelerometer/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.accelerometer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var seekBar: SeekBar
+    private var seekBarValue: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        seekBar = findViewById(R.id.moveSeekBar)
+        seekBarValue = seekBar.progress
     }
 }
